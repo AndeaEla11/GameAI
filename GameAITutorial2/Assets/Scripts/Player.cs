5e175f0f75fed7a0373e4c97a7afcdaf8012a96a
Andreea Straulescu <andreaela99@gmail.com>
2025-11-21 02:45:50 +0000

    V003

--- GameAITutorial2/Assets/Scripts/Player.cs
+++ GameAITutorial2/Assets/Scripts/Player.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
 
     public float speed;
+    
+
+    [HideInInspector]
+    public int health = 100;
+    public Text PlayerHPText;
+
+
     private CharacterController controller; 
 
     
@@ -22,11 +31,34 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(x, 0f, z);
 
         if (direction.magnitude > 1f)
-        {
             direction = direction.normalized;
-        }
+
 
         controller.Move(direction * speed * Time.deltaTime);
+
+
+    }
+
+    public void TakeDamege(int damage)
+    {
+        health -= damage;
+
+        UpdateHPUI(); 
+
+        if (health <= 0)
+            PlayerDied();
+
+
+    }
+
+    void UpdateHPUI()
+    {
+        PlayerHPText.text = "HP: " + health; 
+    }
+
+    void PlayerDied()
+    {
+        gameObject.SetActive(false);
     }
 
 }
